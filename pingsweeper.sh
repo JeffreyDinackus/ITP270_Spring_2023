a93ee97d4f95b4f0250d5a9ef9072168fe877f5b
JeffreyDinackus <z3l9m@protonmail.com>
2023-02-16 20:32:14 -0500

    more pingsweeper additions

--- pingsweeper.sh
+++ pingsweeper.sh
@@ -5,11 +5,11 @@ max1=255
 max2=255
 
 
-for i in `seq 2 $max1`
+for i in `seq 1 $max1`
 do
-    for h in `seq 2 $max2`
+    for h in `seq 1 $max2`
     do
-        var=192.168.$i.$h
-        ping $var
+        var=192.168.58.$h
+        ping -c 1 $var
 done
 done
