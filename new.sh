adebb917200d8a13c5316ee1c2e028db51132484
JeffreyDinackus <z3l9m@protonmail.com>
2023-02-26 16:55:40 -0500

    new script new.sh for learning shell scripting

---
+++ new.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#Different shells handle things slightly differently.
+
+CustomerName=Jedidiah
+#it won't work if you have spaces in zsh, it may work with the spaces in other bash shells. 
+echo $CustomerName
+
+#this is a varaible that is also a number. 
+declare -i PhoneNumber=5042030234
+
+#this is a constant. -r means read-only. 
+declare -r Pi=3.14
+
+echo $Pi
+
+#this is how to make a array
+tempArray=(1,2,3,4,5)
+echo $tempArray
+
+#this is called a associatiave array, it might be like a object.
+declare -A PhoneBook
+PhoneBook[name]="Humus"
+PhoneBook[number]="444-3029"
+
+printf ${PhoneBook[name]}
+printf ${PhoneBook[number]}
+
